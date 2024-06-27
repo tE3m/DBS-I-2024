039ced4738fed0e73ec19820500d8fb8a26fa041
Tarek Massini <tarek@massini.me>
2024-06-27 15:27:48 +0200

    Alles fertig

--- src/main/java/JDBCExerciseJavaImplementation.java
+++ src/main/java/JDBCExerciseJavaImplementation.java
@@ -50,7 +50,26 @@ public class JDBCExerciseJavaImplementation implements JDBCExercise {
 	) throws SQLException {
 		logger.info(keywords);
 		List<Actor> actors = new ArrayList<>();
-
-		throw new UnsupportedOperationException("Not yet implemented");
+		PreparedStatement actors_statement = connection.prepareStatement("SELECT nconst, primaryname FROM nbasics NATURAL JOIN tprincipals WHERE primaryname LIKE ? AND (category='actor' OR category='actress') GROUP BY nconst, primaryname ORDER BY COUNT(DISTINCT tconst) DESC, primaryname LIMIT 5");
+		PreparedStatement movies_statement = connection.prepareStatement("SELECT \"primaryTitle\" FROM tmovies NATURAL JOIN tprincipals WHERE nconst = ? AND (category ='actor' OR category = 'actress') ORDER BY \"startYear\" DESC, \"primaryTitle\" NULLS LAST LIMIT 5");
+		PreparedStatement costars_statement = connection.prepareStatement("WITH filme AS (SELECT DISTINCT tconst FROM tprincipals WHERE nconst = ?) SELECT primaryname, COUNT(DISTINCT tconst) AS anzahl FROM tprincipals NATURAL JOIN filme NATURAL JOIN nbasics WHERE nconst != ? AND (category = 'actor' OR category = 'actress') GROUP BY nconst, primaryname ORDER BY anzahl DESC, primaryname LIMIT 5");
+		actors_statement.setString(1, "%" + keywords + "%");
+		ResultSet actors_results = actors_statement.executeQuery();
+		while (actors_results.next()) {
+			movies_statement.setString(1, actors_results.getString("nconst"));
+			costars_statement.setString(1, actors_results.getString("nconst"));
+			costars_statement.setString(2, actors_results.getString("nconst"));
+			ResultSet movies_results = movies_statement.executeQuery();
+			ResultSet costars_results = costars_statement.executeQuery();
+			Actor new_actor = new Actor(actors_results.getString("nconst"), actors_results.getString("primaryname"));
+			while (movies_results.next()) {
+				new_actor.playedIn.add(movies_results.getString("primaryTitle"));
+			}
+			while (costars_results.next()) {
+				new_actor.costarNameToCount.put(costars_results.getString("primaryName"), costars_results.getInt("anzahl"));
+			}
+			actors.add(new_actor);
+		}
+		return actors;
 	}
 }
